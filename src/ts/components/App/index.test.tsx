fd2c8b297efc3c9f28269dab59820ee5f722ca43
Luca Valsecchi <luca.valsecchi@hotelscan.com>
2021-10-04 14:15:10 +0200

    feat(boilerplate): clear boilerplate from useless tests and code

--- src/ts/components/App/index.test.tsx
+++ src/ts/components/App/index.test.tsx
@@ -3,7 +3,7 @@
  */
 
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import App from "../App";
 
@@ -11,18 +11,5 @@ describe("<App />", () => {
     const {container: app} = render(<App />);
     test("App matches the snapshot", () => {
         expect(app).toMatchSnapshot();
-    });
-    test("App text expected to be the correct one", () => {
-        const { getByText } = render(<App />)
-        expect(getByText("This is a boilerplate")).toBeInTheDocument()
-    });
-});
-
-describe("H1 visibility", () => {
-    test("Bootstrapping boilerplate, title is hidden and after click on the button, it's visible", () => {
-        render(<App />);
-        expect(screen.queryByTestId("title")).toBeNull();
-        fireEvent.click(screen.getByTestId("button"));
-        expect(screen.getByText("Reactssup title")).toBeVisible()
     });
 });
--- src/ts/components/Title/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from "react"; 
-import { ITitleProps } from "./index.d";
-
-const Title: FC<ITitleProps> = ({text}) => <div>{text}</div>
-
-export default Title;
